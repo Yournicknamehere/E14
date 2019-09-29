4f7ecff0dc8b22946b0cb6030861357d8326a2ac
Yournicknamehere <35414903+Yournicknamehere@users.noreply.github.com>
2019-09-29 11:26:19 +0200

    Zmienione ścieżki do CSS i JS

--- dodaj.php
+++ dodaj.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="style.css">
-    <script src="main.js"></script>
+    <link rel="stylesheet" href="/CSS/style.css">
+    <script src="/JS/main.js"></script>
     <title>Dodano</title>
 </head>
 <body>
