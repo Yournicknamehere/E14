ff34c6e83a7e0d4846b9e2492aed847cd4418342
Yournicknamehere <czadur@gmail.com>
2019-11-13 12:41:10 +0100

    Poprawki

--- dodawanie.php
+++ dodawanie.php
@@ -14,16 +14,41 @@
     </div>
 
     <div class="content">
-        <form action="dodaj.php" method="POST">
+        <form action="<?php echo $_SERVER["PHP_SELF"];?>" method="POST">
             <input type="text" name="nazwa" class="formInput" placeholder="Nazwa"/><br>
             <input type="text" name="adres" class="formInput" placeholder="Adres"/><br>
             <input type="text" name="miasto" class="formInput" placeholder="Miasto"/><br>
             <input type="text" name="kraj" class="formInput" placeholder="Kraj"/><br>
             <br>
-            <input type="submit" name="submitBtn" value="Dodaj" class="formInputBtn"/>
+            <input type="submit" name="submitDodaj" value="Dodaj" class="formInputBtn"/>
         </form>
-        <!-- Cofnięcie do poprzedniej strony używając PHP -->
-        <button class="formInputBtn" id="confnijBtn"><a href ="<?php echo $_SERVER['HTTP_REFERER'];?>">Cofnij</a></button>
+
+        <?php 
+            $connection = new mysqli('localhost', 'root', '', 'cd4ti');
+            if($connection->connect_error) { die("Błąd połączenia: " .$connection->connect_error); }
+
+            if(isset($_POST['submitDodaj'])){
+                $nazwa = trim($_POST['nazwa']);
+                $adres = trim($_POST['adres']);
+                $miasto = trim($_POST['miasto']);
+                $kraj = trim($_POST['kraj']);
+
+                if(empty($nazwa) || empty($adres) || empty($miasto) || empty($kraj)) {
+                    echo "<script> alert('Nie dodano klienta, ponieważ co najmniej jedno pole nie zostało uzupełnione.'); </script>";
+                }
+                else {
+                    $sql = "INSERT INTO klienci (nazwa, adres, miasto, kraj) 
+                    VALUES ('$nazwa', '$adres', '$miasto', '$kraj');";
+
+                    if($connection->query($sql) === true) {
+                        echo "<script> alert('Pomyślnie dodano klienta: $nazwa'); </script>";
+                    }else { echo "ERROR: " .$sql ."<br>" .$connection->error; }
+                }
+                $nazwa = $adres = $miasto = $kraj = $sql = "";
+            }
+            $connection->close();
+         ?>
+        <button class="formInputBtn" id="confnijBtn"><a href ="index.php">Cofnij</a></button>
     </div>
     
 </body>
