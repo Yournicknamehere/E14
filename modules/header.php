41508547343534fe183fa6847215439bda2b9073
Yournicknamehere <czadur@gmail.com>
2020-02-24 08:35:03 +0100

    Update header.php

--- modules/header.php
+++ modules/header.php
@@ -1,8 +1,9 @@
 <?php
     $title = stripslashes($_SERVER["PHP_SELF"]);
-    $title = stripcslashes($title);
+    $t = str_ireplace(".php", "", $title);
+    $t = str_ireplace("/", "", $t);
     echo '
-        <a href="echo $_SERVER["PHP_SELF"];" class="logo">' .$title .'</a>
+        <a href="echo $_SERVER["PHP_SELF"];" class="logo">' .$t .'</a>
         <div class="header-right">
             <a href="#sidebar" id="openNav" class="openNav" onclick="openNav()">☰</a>
         </div>
